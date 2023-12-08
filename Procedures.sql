0a7fac05c76b9a21fa65e11746a6ed5fec4250ef
Silvio Virtuoso Júnior <141785530+Virtuosinh0@users.noreply.github.com>
2023-12-07 23:57:30 -0300

    Update Procedures.sql
    Deletada procedures incorretas conforme a modificação no banco

--- Procedures.sql
+++ Procedures.sql
@@ -1,21 +1,6 @@
-CREATE PROCEDURE pr_atualiza_valor (@pedido INT) AS
-BEGIN
-	DECLARE @novoPrecoTotal NUMERIC(5,2);
-
-	SELECT @novoPrecoTotal = SUM(M.preco * PM.quantidade)
-	FROM pedido_menu AS PM
-	INNER JOIN Menu AS M ON M.codigo_menu = PM.codigo_menu
-	WHERE PM.codigo_pedido = @pedido;
-
-	UPDATE pedido
-	SET preco_total = ISNULL(@novoPrecoTotal, 0)
-	WHERE codigo_pedido = @pedido;
-END
-GO
-
 CREATE PROCEDURE pr_cria_pedido (@menu INT, @quantidade INT, @pedido INT OUTPUT) AS
 BEGIN
-	INSERT INTO pedido VALUES (GETDATE(), 0);
+	INSERT INTO pedido VALUES (GETDATE(), 0, NULL);
 	SELECT @pedido = SCOPE_IDENTITY();
 
 	INSERT INTO pedido_menu VALUES (@menu, @pedido, @quantidade);
@@ -29,20 +14,7 @@ BEGIN
 	EXEC pr_atualiza_valor @pedido;
 END
 GO
-
-CREATE PROCEDURE pr_remove_pedido (@menu INT, @pedido INT) AS
-BEGIN
-	DELETE FROM pedido_menu WHERE codigo_menu = @menu AND codigo_pedido = @pedido;
-	EXEC pr_atualiza_valor @pedido;
-END
-GO
-
-CREATE PROCEDURE pr_corrige_pedido (@menu INT, @quantidade INT, @pedido INT) AS
-BEGIN
-	UPDATE pedido_menu SET quantidade = @quantidade WHERE codigo_pedido = @pedido AND codigo_menu = @menu
-	EXEC pr_atualiza_valor @pedido;
-END
-GO
+	
 CREATE PROCEDURE pr_deleta_pedido (@pedido INT) AS
 BEGIN
 	DELETE FROM pedido_menu WHERE codigo_pedido = @pedido;
