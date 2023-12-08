500367de14b6bd1bd5e15e35acfb6555b70234bd
Silvio Virtuoso Júnior <141785530+Virtuosinh0@users.noreply.github.com>
2023-12-07 23:54:42 -0300

    Update Funcoes.sql
    Corrigida a função e adicionada uma nova

--- Funcoes.sql
+++ Funcoes.sql
@@ -4,15 +4,34 @@ AS
 RETURN
 (
     SELECT 
-        P.codigo_pedido,
+        P.cod_pedido,
         M.nome AS nome_item,
         PM.quantidade * M.preco AS preco_total_item
     FROM 
         pedido_menu AS PM
     INNER JOIN 
-        menu AS M ON M.codigo_menu = PM.codigo_menu
+        menu AS M ON M.cod_item_menu = PM.cod_item_menu
     INNER JOIN 
-        pedido AS P ON P.codigo_pedido = PM.codigo_pedido
+        pedido AS P ON P.cod_pedido = PM.cod_pedido
     WHERE 
-        PM.codigo_pedido = @pedido
+        PM.cod_item_menu = @pedido
+);
+
+CREATE FUNCTION fc_visualiza_receita (@menu INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT 
+        M.nome AS Menu,
+		P.nome AS Produto,
+        RM.quantidade_produto AS Quantidade
+    FROM 
+        receita_item_menu AS RM
+    INNER JOIN 
+        menu AS M ON M.cod_item_menu = RM.cod_item_menu
+    INNER JOIN 
+        produto AS P ON P.cod_produto = RM.cod_produto
+    WHERE 
+        RM.cod_item_menu = @menu
 );
