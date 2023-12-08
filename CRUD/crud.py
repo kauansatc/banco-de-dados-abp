16a0f38b19f667bf495d973c0360ed40f4e843c1
kauanfontanela <kauanfontanela09@gmail.com>
2023-12-08 11:33:37 -0300

    change CRUD to azure db

--- CRUD/crud.py
+++ CRUD/crud.py
@@ -1,26 +1,23 @@
-import mysql.connector
-from mysql.connector import Error
+import pyodbc
+server = 'r8.database.windows.net'
+database = 'dados'
+username = 'r8'
+password = 'satc@2023'
+driver = '{ODBC Driver 17 for SQL Server}'
 
-class Database:
-    connection: mysql.MySQLConnection
 
-    def __init__(self, **connection_args):
-        try:
-            self.connection = mysql.connector.connect(**connection_args)
-            print("Connected to MySQL Server version ", self.connection.get_server_info())
-        except Error as err:
-            print("Error while connecting to MySQL", err)
-            # REVIEW raise err
-            
-    def __read(self, query):
-        cursor = self.connection.cursor()
-        res = None
-        try:
-            cursor.execute(query)
-            res = cursor.fetchall()
-            return res
-        except:
-            raise # TODO feedback do erro
-    
-    def get_all_items(self):
-        return self.__read("SELECT * FROM item")
+class __Database:
+    def __init__(self):
+        self.conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server +
+                                   ';PORT=1433;DATABASE='+database+';UID='+username+';PWD=' + password)
+
+        if self.conn:
+            print("Conectado com sucesso!")
+
+    def get_menu(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM menu")
+            return cursor.fetchall()
+
+
+database = __Database()
